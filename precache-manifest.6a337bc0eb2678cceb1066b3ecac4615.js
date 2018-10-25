76ddd2d00ef3161020c02c3a0f10ea740563ad4f
Rahul Jha <rahulkumarjha26@gmail.com>
2018-10-25 10:54:39 +0530

    Updates

---
+++ precache-manifest.6a337bc0eb2678cceb1066b3ecac4615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a73abdcfa4072db5abd",
+    "url": "/facerecognition/static/js/runtime~main.1a73abdc.js"
+  },
+  {
+    "revision": "b33b043164ba20a1d9af",
+    "url": "/facerecognition/static/js/main.b33b0431.chunk.js"
+  },
+  {
+    "revision": "8a284307574a8113099e",
+    "url": "/facerecognition/static/js/1.8a284307.chunk.js"
+  },
+  {
+    "revision": "b33b043164ba20a1d9af",
+    "url": "/facerecognition/static/css/main.1470d860.chunk.css"
+  },
+  {
+    "revision": "8a284307574a8113099e",
+    "url": "/facerecognition/static/css/1.9a35d20e.chunk.css"
+  },
+  {
+    "revision": "9489c4ba0fcce97369ca07106e30e17a",
+    "url": "/facerecognition/index.html"
+  }
+];
